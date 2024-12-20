36b7873252282c34c2936a1b62b107dfcc33e5eb
Shishir <shishirsrivastava506@gmail.com>
2024-12-20 23:19:56 +0530

    added files

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,11 @@
+import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
+import { Inter } from 'next/font/google'
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
+import { shadesOfPurple } from "@clerk/themes";
+
+const inter = Inter({ subsets: ["latin"]}); 
 
 export const metadata = {
   title: "Create Next App",
@@ -7,10 +14,25 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        {children}
+      <ClerkProvider
+
+      appearance={{
+        baseTheme:shadesOfPurple
+      }}
+      >
+    <html lang='en'>
+      <body className={`${inter.className }`}>
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <Header/>
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-gray-800 py-12">
+            <div className=" container mx-auto px-4 text-center text-gray-500">
+            <p>Made by shishir</p>
+            </div>
+          </footer>
+          </ThemeProvider>
       </body>
     </html>
+      </ClerkProvider>
   );
 }
