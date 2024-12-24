0f1a2f7551eff426c34aa3e194e2451b7f18036d
Shishir <shishirsrivastava506@gmail.com>
2024-12-24 18:38:13 +0530

    bug fix

--- components/header.jsx
+++ components/header.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import { PenBox } from "lucide-react";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="container mx-auto">
+      <nav className="py-6 px-4 flex justify-between items-center">
+        <Link href="/">
+          <Image
+            src={"/logo2.png"}
+            width={200}
+            height={56}
+            alt="logo"
+            className="h-10 w-auto object-contain"
+          />
+        </Link>
+        <div>
+          <Link href="/project/create">
+            <Button variant="destructive" className="items-center flex gap-2">
+              <PenBox size={18}/>
+              <span>Create Project</span>
+            </Button>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- middleware.js
+++ middleware.js
@@ -1,6 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server"; 
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"; 
 
-export default clerkMiddleware();
+const isProtectedRoute = createRouteMatcher([
+  "/onboarding(.*)",
+  "/organisation(.*)",
+  "/project(.*)",
+  "/issue(.*)",
+  "/sprint(.*)",
+]);
+
+export default clerkMiddleware((auth,req) =>{
+  if(!auth().userId && isProtectedRoute(req)){
+    return auth().redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
